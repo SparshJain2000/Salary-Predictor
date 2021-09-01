475cced8e0bb5ca0196ed8b08798a317cc20fec9
SparshJain2000 <jainsparsh0801@gmail.com>
2021-09-02 00:44:20 +0530

    add loader on API call

--- client/src/pages/predict.pages.jsx
+++ client/src/pages/predict.pages.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Layout } from "antd";
 import "../styles/predict.scss";
 import { Form, Button, Select, Row, Col, InputNumber } from "antd";
@@ -8,17 +8,24 @@ const required = {
     required: true,
     message: "Please provide input!",
 };
-
-const Predict = () => {
+const Predict = ({ setloading }) => {
+    const result = useRef(null);
     const [salary, setsalary] = useState(null);
     const onFinish = async (values) => {
+        setloading(true);
         console.log("Success:", values);
-        let { data } = await axios.post(
-            "http://localhost:5000/api/predict",
-            values,
-        );
-        console.log(data);
-        setsalary(Number(data.salary / 100).toFixed(2));
+        try {
+            let { data } = await axios.post(
+                "http://localhost:5000/api/predict",
+                values,
+            );
+            console.log(data);
+            setsalary(Number(data.salary / 100).toFixed(2));
+            result.current.scrollIntoView({ behavior: "smooth" });
+        } catch (e) {
+            console.log(e?.response?.data?.error);
+        }
+        setloading(false);
     };
 
     const onFinishFailed = (errorInfo) => {
@@ -43,7 +50,14 @@ const Predict = () => {
                             name='country'
                             rules={[required]}
                             hasFeedback>
-                            <Select>
+                            <Select
+                                filterOption={(input, option) =>
+                                    option.children
+                                        .toLowerCase()
+                                        .indexOf(input.toLowerCase()) >= 0
+                                }
+                                showSearch
+                                placeholder='Select a Country'>
                                 {data.country.map((val) => (
                                     <Select.Option value={val}>
                                         {val}
@@ -58,7 +72,7 @@ const Predict = () => {
                             name='degree'
                             rules={[required]}
                             hasFeedback>
-                            <Select>
+                            <Select placeholder='Select a Degree'>
                                 {data.degree.map((val) => (
                                     <Select.Option value={val}>
                                         {val}
@@ -73,7 +87,7 @@ const Predict = () => {
                             name='employment'
                             rules={[required]}
                             hasFeedback>
-                            <Select>
+                            <Select placeholder='Select employment type'>
                                 {data.employment.map((val) => (
                                     <Select.Option value={val}>
                                         {val}
@@ -88,7 +102,14 @@ const Predict = () => {
                             name='ug'
                             rules={[required]}
                             hasFeedback>
-                            <Select>
+                            <Select
+                                filterOption={(input, option) =>
+                                    option.children
+                                        .toLowerCase()
+                                        .indexOf(input.toLowerCase()) >= 0
+                                }
+                                showSearch
+                                placeholder='Select a Major'>
                                 {data.ug.map((val) => (
                                     <Select.Option value={val}>
                                         {val}
@@ -112,6 +133,7 @@ const Predict = () => {
                             <InputNumber
                                 style={{ width: "100%" }}
                                 type='number'
+                                placeholder='Enter your experience (in years)'
                             />
                         </Form.Item>
                     </Col>
@@ -125,7 +147,7 @@ const Predict = () => {
                 </Row>
             </Form>
             {salary && (
-                <h2 className='text-align-center'>
+                <h2 className='text-align-center' ref={result}>
                     Expected salary 💰: ${salary}K
                 </h2>
             )}
