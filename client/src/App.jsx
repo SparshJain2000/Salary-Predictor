0e2ff5e2a58839c838211180c11d4ca7f91f685e
SparshJain2000 <jainsparsh0801@gmail.com>
2021-09-02 00:44:04 +0530

    add loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SideBar from "./components/sidebar.component.jsx";
-import { Layout, Content } from "antd";
+import { Layout } from "antd";
 import Predict from "./pages/predict.pages";
 import Overview from "./pages/overview.pages";
+import Loader from "./components/loader.component";
+
 function App() {
+    const [loading, setloading] = useState(false);
+
     return (
         <BrowserRouter>
+            {loading && <Loader />}
             <Layout>
                 <SideBar />
                 <Layout.Content>
                     <Switch>
-                        <Route path='/' exact component={Predict} />
+                        <Route
+                            path='/'
+                            exact
+                            render={(props) => (
+                                <Predict {...props} setloading={setloading} />
+                            )}
+                        />
                         <Route path='/overview' exact component={Overview} />
                     </Switch>
                 </Layout.Content>
